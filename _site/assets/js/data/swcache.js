1dcdc0b74613fc432672cfc32e22ef58da9924b8
Bartosz Kamiński <k4minski2@gmail.com>
2021-11-26 13:59:42 +0100

    test

---
+++ _site/assets/js/data/swcache.js
@@ -0,0 +1,60 @@
+const resource = [
+
+  /* --- CSS --- */
+  '/-kvminski-.github.io/assets/css/style.css',
+
+  /* --- JavaScripts --- */
+  
+  '/-kvminski-.github.io/assets/js/dist/home.min.js',
+  '/-kvminski-.github.io/assets/js/dist/page.min.js',
+  '/-kvminski-.github.io/assets/js/dist/post.min.js',
+  '/-kvminski-.github.io/assets/js/dist/categories.min.js',
+  '/-kvminski-.github.io/assets/js/data/search.json',
+  '/-kvminski-.github.io/app.js',
+  '/-kvminski-.github.io/sw.js',
+
+  /* --- HTML --- */
+  '/-kvminski-.github.io/index.html',
+  '/-kvminski-.github.io/404.html',
+  
+    '/-kvminski-.github.io/categories/',
+  
+    '/-kvminski-.github.io/tags/',
+  
+    '/-kvminski-.github.io/archives/',
+  
+    '/-kvminski-.github.io/about/',
+  
+
+  /* --- Favicons --- */
+  
+
+  '/-kvminski-.github.io/assets/img/favicons/android-chrome-192x192.png',
+  '/-kvminski-.github.io/assets/img/favicons/android-chrome-512x512.png',
+  '/-kvminski-.github.io/assets/img/favicons/apple-touch-icon.png',
+  '/-kvminski-.github.io/assets/img/favicons/favicon-16x16.png',
+  '/-kvminski-.github.io/assets/img/favicons/favicon-32x32.png',
+  '/-kvminski-.github.io/assets/img/favicons/favicon.ico',
+  '/-kvminski-.github.io/assets/img/favicons/mstile-150x150.png',
+  '/-kvminski-.github.io/assets/img/favicons/site.webmanifest',
+  '/-kvminski-.github.io/assets/img/favicons/browserconfig.xml'
+
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  'kvminski.github.io',
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [
+  
+];
+
